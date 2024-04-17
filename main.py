e902f6551fdaeee411713c465e5e3dc3b1044a0d
Luix <luiz.delgado@eniac.edu.br>
2024-04-17 16:50:44 -0300

    Desenvolvimento do desafio 2 de IoT

---
+++ main.py
@@ -0,0 +1,40 @@
+#Passo 1 - Importar as bibliotecas
+import paho.mqtt.client as mqtt #Biblioteca do protocolo MQTT
+import time #Biblioteca responsável pelos "delays" do código
+
+#Passo 2 - Instanciar conexão
+
+#Credenciais de acesso ao servidor
+BROKER = "192.168.2.102" #177.8.167.89" - IP EXTERNO
+PORT = 7500
+USERNAME = "planta40mqttlogin"
+PASSWORD = "gatoehfofoplanta40eniac"
+TOPIC = "aula_iot_show"
+CLIENT_ID = "Prof Little Luiz"
+
+#Função de conexão
+def on_connect(client, userdata,flgs, rc:int):
+    if rc==0:
+        print("Conexão bem sucedida ao servidor")
+    else:
+        print("Não foi possível se conectar ao servidor")
+
+def on_message(client, userdata, mensagem):
+    print(f"A mensagem foi recebida no tópico {mensagem.topic} foi {mensagem.payload.decode()}")
+
+#Passo 3 - Criar o cliente
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, client_id=CLIENT_ID)
+
+#Passo 4 - Configurar a conexão com o servidor
+client.on_connect = on_connect
+client.on_message = on_message
+
+#Passo 5 - looping de leitura
+client.username_pw_set(USERNAME, PASSWORD)
+client.connect(BROKER, PORT)
+client.subscribe(topic=TOPIC)
+client.publish(TOPIC, "Oi gente, me conectei!")
+
+if __name__ == '__main__':
+    client.loop_forever()
+    
